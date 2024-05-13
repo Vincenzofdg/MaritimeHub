23feddbc798411029087e3e517532488c06a0c11
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-05-13 20:19:22 -0300

    feat: done

--- actions/vessels_in_moviment.py
+++ actions/vessels_in_moviment.py
@@ -2,16 +2,21 @@ from tools.selenium_functions import xpath, tag_names
 import web_elements.vessels_in_moviment as style
 import time
 import re
+from datetime import datetime
 
 class Vessel_Status:
-    def __init__(self, name, agent, status):
+    def __init__(self, name, date, local, status):
         self.name = name
-        self.agent = agent
+        self.date = date
+        self.local = local
         self.status = status
 
 
 def vessels_in_moviment(cursor, browser):
-    sql_query = "INSERT INTO vessels (name, agent, status_rgb) VALUES (%s, %s, %s)"
+    current_date = datetime.now()
+    date_formated = current_date.strftime("%d-%m-%Y - %H:%M:%S")
+
+    sql_query = "INSERT INTO vessels (name, date, local, status_rgb, updated) VALUES (%s, %s, %s, %s, %s)"
 
     vessel_parent = xpath(style.vessel_array, browser)
     vessel_sons = tag_names("tr", vessel_parent)
@@ -26,7 +31,8 @@ def vessels_in_moviment(cursor, browser):
         if index >= 1:
             vessel = Vessel_Status(
                 name = '', 
-                agent = '', 
+                date = '',
+                local = '',
                 status = find_background_color.group(1)
             )
 
@@ -34,8 +40,16 @@ def vessels_in_moviment(cursor, browser):
             for i, info in enumerate(all_info):
                 if i == 1:
                     vessel.name = info.text
-                if i == 8:
-                    vessel.agent = info.text
+                if i == 3:
+                    if len(info.text) > 1:
+                        vessel.local = info.text
+                if i == 4:
+                    if len(info.text) > 1:
+                        vessel.local = info.text
+                if i == 5:
+                    vessel.date = info.text
+
+
             time.sleep(1)
 
-            cursor.execute(sql_query, [vessel.name, vessel.agent, vessel.status])
+            cursor.execute(sql_query, [vessel.name, vessel.date, vessel.local, vessel.status, date_formated])
