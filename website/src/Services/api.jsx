85555fb406ebb7dad08327c1a500828847d2deb7
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-05-23 05:17:19 -0300

    feat: fix_2

--- website/src/Services/api.jsx
+++ website/src/Services/api.jsx
@@ -7,8 +7,8 @@ const {url, prefix, dev} = {
 }
 
 const api = axios.create({
-  baseURL: url + prefix,
-  // baseURL: dev + prefix,
+  // baseURL: url + prefix,
+  baseURL: dev + prefix,
 });
 
 export default api;
